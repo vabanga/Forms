990c34f6d71924cb4e39c5abf10414c6995c786e
goha <gohavorsin@gmail.com>
2017-09-06 21:26:46 +1000

    end

--- admin.php
+++ admin.php
@@ -1,18 +1,22 @@
 <?php
 
 if($_FILES['userfile']){
+    $des = 'tests/';
     if(empty($des)){
-        echo 'Пустая переменная $des';
+        echo 'Пустая переменная';
         die;
     }
-    $des = 'tests/';
     $file = $_FILES['userfile'];
     define(PATH_UPLOAD,'tests');
     $ext = explode(".", $file['name']);
     $uploadfile = $des . basename($_FILES['userfile']['name']);
     if($ext[1]=='json'){
-        move_uploaded_file($file["tmp_name"],$uploadfile);
-        echo"Файл отправлен";
+        $res = move_uploaded_file($file["tmp_name"],$uploadfile);
+        if($res == true){
+            echo"Файл отправлен";
+        }else{
+            echo"Файл НЕ отправлен";
+        }
     }else{
         echo"Файл НЕ отправлен";
     }
